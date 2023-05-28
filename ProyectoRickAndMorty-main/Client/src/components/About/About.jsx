ca29e43007455736ca7b41d9a6971c4776214475
Iván Monte <ivanmonte07.93@gmail.com>
2023-05-28 20:30:00 -0300

    Changes added in about

--- ProyectoRickAndMorty-main/Client/src/components/About/About.jsx
+++ ProyectoRickAndMorty-main/Client/src/components/About/About.jsx
@@ -1,14 +1,24 @@
 import React from "react"
+import {Ivan} from '../About/About.styled.jsx';
 
 const About = () =>{
 
+
 return(
-    <div>
-        <h1>SOY ABOUT</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti sunt veniam voluptate veritatis. Ipsam ex sed id? Molestias natus fugiat nam! Laboriosam dolorem iste eum minima est repudiandae deleniti placeat.</p>
+    <Ivan>
+        <h1>Sobre Nosotros</h1>
+        <h2>GRUPO 8</h2>
+        <h3>Sebastian - Iván - Marcelo</h3>
+        <p>Somos un equipo de tres desarrolladores en Codo a Codo 4.0 buscando obtener nuestra certificacion Python Full Stack</p>
         <br />
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam aut eveniet incidunt veniam. Eum natus pariatur recusandae dolor, magni veniam ipsam quisquam aliquam a expedita itaque facilis! Tenetur, deserunt voluptatibus!</p>
-    </div>
+        <p>Esta web se trata de un proyecto para aplicar nuestros conocimientos y habilidades obtenidas durante el curso reflejando informacion sobre Rick y Morty.
+            Como desarrolladores tenemos como objetivo concretar sitios web, y aplicaciones para brindar la mejor experiencia al usuario al navegar por la web o simplemente usar alguna aplicacion. 
+            <br />
+            Otro objetivo que tenemos es desarrollar simplicidad, inclusividad y estar entre las primeras busquedas al momento en que las referencias por busquedas sean en el internet. 
+        </p>
+        <br />
+        <p>Es una web donde puedes conocer el nombre de los personajes, sus estados y caracterisitcas. </p>
+    </Ivan>
 )
 }
 export default About
---
+++ ProyectoRickAndMorty-main/Client/src/components/About/About.styled.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Ivan = styled.div`
+    
+    color: violet;
+    font-size: 30px;
+    font-family: 'Nothing You Could Do', cursive;
+    `
